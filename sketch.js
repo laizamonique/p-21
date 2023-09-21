131bc83dae4c6e9652bfb4eaabf7853b125ea39e
laizamonique <133267385+laizamonique@users.noreply.github.com>
2023-09-21 17:01:46 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,18 +4,43 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
+var ball, groundObj ,leftside, rightside;
+var world;
+var radius = 40;
+
 function preload()
 {
+
 	
 }
 
 function setup() {
-	createCanvas(800, 700);
-
+	createCanvas(1000, 700);
+        rectMode(center);
 
 	engine = Engine.create();
-	world = engine.world;
 
+world = engine.world;
+
+var ball_options={
+
+isStatic: false,
+
+restitution: 0.3,
+
+friction:0,
+
+density: 1.2
+}
+ ball = Bodies.circle(260, 100, radius/2, ball_options);
+
+World.add(world, ball);
+
+groundObj = new ground (width/2,670, width, 20);
+
+leftSide = new ground (1100, 600, 20, 120); rightSide = new ground (1350, 600, 20, 120);
+
+       
 	//Create the Bodies Here.
 
 
@@ -27,10 +52,18 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  
+  ellipse(ball.position.x,ball.position.y, radius, radius);
   drawSprites();
- 
+  groundObj.display();
+
+leftSide.display();
+
+rightSide.display();
 }
+function keyPressed() {
 
-
+if (keyCode === UP_ARROW) {
+Matter.Body.apply Force (ball, ball.position, {x:85,y: -85});
+}
+}
 
